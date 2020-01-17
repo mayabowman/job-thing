b17b6818f6f91f0bf502c968ea8683f38a43314c
Maya Bowman <mayashady1@gmail.com>
2020-01-17 17:21:13 -0500

    working on getJobByUserId

--- src/components/JobList/JobList.js
+++ src/components/JobList/JobList.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import dummyStore from '../../dummy-store';
+// import dummyStore from '../../dummy-store';
 import JobListing from '../JobListing/JobListing';
 import UpdateJobContext from '../../contexts/UpdateJobContext';
+import JobsApiService from '../../services/jobs-api-service';
+import TokenService from '../../services/token-service';
 
 class JobList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null
+    }
+  }
+
   static contextType = UpdateJobContext
+
+  componentDidMount() {
+    let userId = Number(TokenService.getUserId())
+    JobsApiService.getDataForUser(userId)
+      .then(data => {
+        this.context.setJobs(data)
+      })
+      .catch(error => {
+        this.setState({
+          error: error
+        })
+      })
+  }
+
   render() {
-    const jobs = dummyStore.jobs
+    // const jobs = dummyStore.jobs
     return (
       <div>
         <h1>Jobs You've Applied For</h1>
         <ul>
-          {jobs.map(i => (
+          {this.context.jobs.map(i => (
             <li key={i.id}>
               <JobListing
                 id={i.id}
